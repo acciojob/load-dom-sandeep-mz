c5893636e5e68da1544b5b77e65cb52b234012a0
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-09-29 15:47:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-//your JS code here. If required.
- document.addEventListener("DOMContentLoaded", ()=> {
-            document.write = "<h2>DOM load success</h2>"
-    });
+function onDOMLoad() {
+  // Check if the body tag has any children
+  if (document.body.childElementCount === 0) {
+    // Create a new paragraph element
+    var paragraph = document.createElement("p");
 
+    // Set the text content of the paragraph
+    paragraph.textContent = "DOM load success";
 
- // Function to execute when the DOM is loaded
-        // function onDOMLoad() {
-        //     // Create a new paragraph element
-        //     var paragraph = document.createElement("p");
-            
-        //     // Set the text content of the paragraph
-        //     paragraph.textContent = "DOM load success";
-            
-        //     // Append the paragraph to the body of the document
-        //     document.body.appendChild(paragraph);
-        // }
+    // Append the paragraph to the body of the document
+    document.body.appendChild(paragraph);
+  }
+}
 
-        // // Attach the onDOMLoad function to the DOMContentLoaded event
-        // document.addEventListener("DOMContentLoaded", onDOMLoad);
+// Attach the onDOMLoad function to the DOMContentLoaded event
+document.addEventListener("DOMContentLoaded", onDOMLoad);
