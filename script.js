540accccf9ae994a3304a003e2791d36284e9eea
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-09-29 15:49:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function onDOMLoad() {
   if (document.body.childElementCount === 0) {
     // Create a new paragraph element
     var paragraph = document.createElement("p");
-
+    
     // Set the text content of the paragraph
     paragraph.textContent = "DOM load success";
-
+    
     // Append the paragraph to the body of the document
     document.body.appendChild(paragraph);
   }
