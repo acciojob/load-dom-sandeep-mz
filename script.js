5995884fbcf1c692aed1124cbe52533cf790eebe
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-09-29 15:39:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 //your JS code here. If required.
- // document.addEventListener("DOMContentLoaded", ()=> {
- //            document.write = "<h2>DOM load success</h2>"
- //    });
+ document.addEventListener("DOMContentLoaded", ()=> {
+            document.write = "<h2>DOM load success</h2>"
+    });
 
 
  // Function to execute when the DOM is loaded
-        function onDOMLoad() {
-            // Create a new paragraph element
-            var paragraph = document.createElement("p");
+        // function onDOMLoad() {
+        //     // Create a new paragraph element
+        //     var paragraph = document.createElement("p");
             
-            // Set the text content of the paragraph
-            paragraph.textContent = "DOM load success";
+        //     // Set the text content of the paragraph
+        //     paragraph.textContent = "DOM load success";
             
-            // Append the paragraph to the body of the document
-            document.body.appendChild(paragraph);
-        }
+        //     // Append the paragraph to the body of the document
+        //     document.body.appendChild(paragraph);
+        // }
 
-        // Attach the onDOMLoad function to the DOMContentLoaded event
-        document.addEventListener("DOMContentLoaded", onDOMLoad);
+        // // Attach the onDOMLoad function to the DOMContentLoaded event
+        // document.addEventListener("DOMContentLoaded", onDOMLoad);
